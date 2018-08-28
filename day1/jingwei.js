45163c5f03da2478fd28555adfb029b4e8d7c57a
JIngwei Kong <56349014@qq.com>
2018-08-28 16:55:15 +0800

    Jingwei day 1 commit

---
+++ day1/jingwei.js
@@ -0,0 +1,19 @@
+// # 日期: 2018/08/28 Tuesday
+// # 创建: G
+// # 提示：Javascript. 
+
+
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich'];
+let fruits = ['Nut', 'ice cream','coconut','orange','lemon','egg fruit']
+
+let g = animals.map(animals => animals[0]);
+let n = fruits.map(fruits => fruits[0]);
+
+console.log(g.join('') + ' ' + n.join(''));
+
+// Feedback area
+
+// Nicole:
+
+
+// G:
